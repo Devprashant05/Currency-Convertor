ed7c3d0c413d60c2df659d054f70720bdc8f8615
Prashant <Prashantsehgal03@gmail.com>
2024-04-12 17:30:31 +0530

    Currency Convertor App completed

---
+++ src/hooks/useCurrencyInfo.js
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from 'axios'
+
+
+
+function useCurrencyInfo() {
+    let url = `https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_Ebn09TkyvhTvTY4c9XZsmwoYp5myB0VoF4GAY4Lx`
+    const [data, setData] = useState({});
+    useEffect(() => {
+        fetch(url).then(res => res.json()).then(result => setData(result.data))
+        // axios.request(url).then(res => {
+        //     return console.log((res.data.data).currency)
+        // })
+    }, [])
+    return data;
+}
+
+
+
+export default useCurrencyInfo
